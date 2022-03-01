02d4819978ba4bc2d8963c6765e2d7fa8d47a29f
Petr Valoušek <Valousek.Petr@stud.vassboskovice.cz>
2022-03-01 11:35:30 +0100

    Přidejte soubory projektu.

---
+++ oop sss auto/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oop_sss_auto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            vehicle auto1 = new vehicle("Škoda");
+            auto1.Type = "Osobní auto";
+
+            vehicle auto2 = new vehicle("VW"); 
+            auto1.Type = "Dodávka";
+        }
+    }
+    
+}
---
+++ oop sss auto/vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_sss_auto
+{
+    public class vehicle
+    {
+        public string Type { get; set; }
+        public string Brand { get;}
+        public string Fuel { get; set; }
+        public double Enginevolume { get; set; }
+        public int FuelLevel { get; set; }
+        public int Distance { get; set; }
+        public int PneuState { get; set; }
+
+        public vehicle(string brand)
+        {
+            Type = "Unknown";
+            Brand = Brand;
+            Fuel = "Gas";
+            Enginevolume = 1000;
+            FuelLevel = 100;
+            Distance = 0;
+            PneuState = 100;
+
+        }
+
+
+    }   
+}
